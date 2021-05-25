8678e2b3466fc17a4123cd722d3a0cdfa7af62cf
Philipp Bönsch <philipp.boensch@softgames.de>
2021-05-25 21:47:20 +0200

    WIP slow down

--- Player.gd
+++ Player.gd
@@ -4,6 +4,7 @@ const GRAVITIY: Vector2 = Vector2(0.0, 30.0)
 const JUMP: Vector2 = Vector2(0.0, -800.0)
 const SPEED: float = 40.0
 var velocity: Vector2
+var slow: bool = false
 
 func _physics_process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_A):
@@ -16,6 +17,13 @@ func _physics_process(delta: float) -> void:
 	
 	if is_on_floor() and Input.is_key_pressed(KEY_SPACE):
 		velocity += JUMP
-		
+	#else:
+	#	slow = not is_on_floor() and Input.is_key_pressed(KEY_SPACE)
+
 	velocity += GRAVITIY
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+func _process(delta: float) -> void:
+	if slow:
+		print("slow")
